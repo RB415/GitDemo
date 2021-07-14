142d4dfb9acfa8ed34c9aaad65cf55575ea398a5
PRIYA <rushikesh.s.barge25@gmail.com>
2021-07-14 09:25:26 +0530

    Architect Indian development

--- src/test/java/EtoE/git.java
+++ src/test/java/EtoE/git.java
@@ -1,6 +1,18 @@
 package EtoE;
 
 public class git {
+	
+	public void rush  ()
+	{
+		System.out.println("ruh");
+	}
+	
+
+	public void git  ()
+	{
+		System.out.println("git");
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
